58798001287a85313f98c13bb028407cd76025dd
thatpissedginger <mauijohnsn357@gmail.com>
2021-04-28 20:08:25 -0700

    testing change

--- installbasicapps.ps1
+++ installbasicapps.ps1
@@ -9,3 +9,13 @@ $swdownloads = "C:\gingeescript"
 
 #Visual Studio Code
 Invoke-WebRequest -uri "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64" -outfile "$swdownloads\vscodeinstall.exe"
+start-process -FilePath "$swdownloads\vscodeinstall.exe" -ArgumentList "/VERYSILENT","/MERGETASKS=!runcode"
+Write-Host "Installing Visual Studio Code"
+
+
+#Firefox
+Invoke-WebRequest -uri "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win64&lang=en-US" -outfile "firefoxinstall.msi"
+$ffinstaller = "$swdownloads\firefoxinstall.msi"
+Start-Process msiexec.exe -ArgumentList "/i $ffinstaller","/qn"
+Write-Host "Installing FireFox "
+
