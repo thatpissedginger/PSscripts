fd2da7e2389ef772fa76c78f821ade5325113c3c
thatpissedginger <mauijohnsn357@gmail.com>
2021-05-01 00:01:15 -0700

    added steam discord and 7zip

--- installbasicapps.ps1
+++ installbasicapps.ps1
@@ -19,3 +19,18 @@ $ffinstaller = "$swdownloads\firefoxinstall.msi"
 Start-Process msiexec.exe -ArgumentList "/i $ffinstaller","/qn"
 Write-Host "Installing FireFox "
 
+# Steam Install shit
+Invoke-WebRequest -uri "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe" -outfile "steaminstaller.exe"
+Start-Process -FilePath "$swdownloads\steaminstaller.exe" -ArgumentList "/s"
+Write-Host "Installing Steam"
+
+
+#discord
+Invoke-WebRequest -uri "https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86" -outfile "discordinstaller.exe"
+Start-Process -FilePath "$swdownloads\discordinstaller.exe"
+Write-Host "Installing Discord"
+
+
+#7-zip
+Invoke-WebRequest -Uri "https://www.7-zip.org/a/7z1900-x64.exe" -OutFile "7zipinstaller.exe"
+Start-Process -FilePath "$swdownloads\7zipinstaller.exe" -ArgumentList "/S","/D="C:\Program Files\7-zip""
