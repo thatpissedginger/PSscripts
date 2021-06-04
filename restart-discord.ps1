a36620633b3af267102187547a329d8fc1760975
thatpissedginger <mauijohnsn357@gmail.com>
2021-06-04 00:11:32 -0700

    change to update.exe so that we dont get console window

--- restart-discord.ps1
+++ restart-discord.ps1
@@ -1,2 +1,2 @@
 start-process taskkill -ArgumentList "/IM discord*","/f" -PassThru -Wait
-start-process -FilePath "$env:LOCALAPPDATA\Discord\app-1.0.9002\Discord.exe"
+start-process -FilePath "$env:LOCALAPPDATA\Discord\Update.exe"
