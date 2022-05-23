cd1852c33a809cbd5ce0105ee38a78f4150966b5
thatpissedginger <mauijohnsn357@gmail.com>
2022-05-22 23:17:39 -0700

    Fixed HKLM path, used the github copilot rec at first

--- disableWU.ps1
+++ disableWU.ps1
@@ -1,7 +1,7 @@
 #Stop Windows update Service then change UseWUServer Registry value to 0 then start windows update
 Get-Service "Windows Update" | Stop-Service
 Write-Host "Windows Update Service Stopped"
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\WindowsUpdate\Settings" -Name "UseWUServer" -Value 0
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" -Value 0
 Write-Host "UseWUServer Registry Value Changed to 0"
 Start-Service "Windows Update"
 Write-Host "Windows Update Service Started"
--- enableWU.ps1
+++ enableWU.ps1
@@ -1,7 +1,7 @@
 #Enable UseWUServer Registry windows update then restart the Windows Update Service
 Stop-Service "Windows Update"
 Write-Host "Windows Update Service Stopped"
-Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\WindowsUpdate\Settings" -Name "UseWUServer" -Value 1
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "UseWUServer" -Value 1
 Write-Host "UseWUServer Registry value updated"
 Start-Service "Windows Update"
 Write-Host "Windows Update Service restarted"
