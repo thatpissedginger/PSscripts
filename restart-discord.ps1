36e49ac4dc430a72bd4277b62bb6a0ef351c56cb
thatpissedginger <mauijohnsn357@gmail.com>
2022-05-22 23:05:20 -0700

    dont need this anymore

--- restart-discord.ps1
+++
@@ -1,3 +0,0 @@
-start-process taskkill -ArgumentList "/IM discord*","/f" -PassThru -Wait -WindowStyle Hidden
-start-process -FilePath "$env:LOCALAPPDATA\Discord\app-1.0.9002\Discord.exe" -WindowStyle Hidden
-exit
